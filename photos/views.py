09fc34822a4652798fc2e564dd5fa9773e43b1d1
placiderapson <placideirandora@gmail.com>
2021-02-05 10:50:09 +0200

    Create initial views

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_gallery(request):
+    return render(request, 'photos/gallery.html')
+
+
+def view_photo(request, pk):
+    return render(request, 'photos/photo.html')
+
+
+def add_photo(request):
+    return render(request, 'photos/add.html')
