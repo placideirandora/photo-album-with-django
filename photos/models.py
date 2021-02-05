fb218c618992031350712715b976a560225fed51
placiderapson <placideirandora@gmail.com>
2021-02-05 20:11:54 +0200

    Create and Register the Models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class Category(models.Model):
+    id = models.CharField(
+        max_length=225,
+        unique=True,
+        blank=False,
+        null=False,
+        default=uuid4,
+        primary_key=True)
+    name = models.CharField(max_length=100, null=False,
+                            blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    id = models.CharField(
+        max_length=225,
+        unique=True,
+        blank=False,
+        null=False,
+        default=uuid4,
+        primary_key=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.description
