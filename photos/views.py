2c1fc78cf256624fd0c2b9e5e1d72d78d9961c2d
placiderapson <placideirandora@gmail.com>
2021-02-05 20:24:14 +0200

    Make the Categories dynamic by retrieving them from the Database

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 
 
 def view_gallery(request):
-    return render(request, 'photos/gallery.html')
+    categories = Category.objects.all()
+    context = {'categories': categories}
+
+    return render(request, 'photos/gallery.html', context)
 
 
 def view_photo(request, pk):
