72716ee9487efa73d40d859bfe06fd45830a106f
placiderapson <placideirandora@gmail.com>
2021-02-05 20:12:14 +0200

    Configure the Static Files URLs

--- photos/urls.py
+++ photos/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 
@@ -7,3 +10,7 @@ urlpatterns = [
     path('photo/<str:pk>', views.view_photo, name='photo'),
     path('add/', views.add_photo, name='add'),
 ]
+
+# Configure the Static Files URLs
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
