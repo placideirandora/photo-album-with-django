bae9821e3e796c88db53eba68de7504085c92a0d
placiderapson <placideirandora@gmail.com>
2021-02-06 09:40:29 +0200

    Implement the Upload Photo functionality

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Photo
 
 # Create your views here.
@@ -22,4 +22,27 @@ def view_photo(request, pk):
 
 
 def add_photo(request):
-    return render(request, 'photos/add.html')
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        # Check Category
+        if 'category' in data:
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(
+                name=data['category_new'])
+        else:
+            category = None
+
+        Photo.objects.create(
+            name=data['name'], category=category,
+            description=data['description'], image=image)
+
+        return redirect('gallery')
+
+    context = {'categories': categories}
+
+    return render(request, 'photos/add.html', context)
